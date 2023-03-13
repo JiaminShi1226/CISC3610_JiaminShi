24d6b5520d5cd73c6912e467749caf837bfaa2bd
JiaminShi1226 <shijiamin0324@gmail.com>
2023-03-13 19:23:18 -0400

    update

---
+++ Animation Project/mario.js
@@ -0,0 +1,187 @@
+mario = {
+  src: "spritesheet.png",
+  img: undefined,
+  frame: 0,
+  offset: 0,
+  frames: [
+    {
+      filename: "tile000.png",
+      frame: {
+        x: 1,
+        y: 1,
+        w: 15,
+        h: 33,
+      },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: {
+        x: 0,
+        y: 0,
+        w: 15,
+        h: 33,
+      },
+      sourceSize: {
+        w: 15,
+        h: 33,
+      },
+    },
+    {
+      filename: "tile001.png",
+      frame: {
+        x: 18,
+        y: 1,
+        w: 21,
+        h: 33,
+      },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: {
+        x: 0,
+        y: 0,
+        w: 21,
+        h: 33,
+      },
+      sourceSize: {
+        w: 21,
+        h: 33,
+      },
+    },
+    {
+      filename: "tile002.png",
+      frame: {
+        x: 41,
+        y: 1,
+        w: 26,
+        h: 33,
+      },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: {
+        x: 0,
+        y: 0,
+        w: 26,
+        h: 33,
+      },
+      sourceSize: {
+        w: 26,
+        h: 33,
+      },
+    },
+    {
+      filename: "tile003.png",
+      frame: {
+        x: 1,
+        y: 36,
+        w: 24,
+        h: 33,
+      },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: {
+        x: 0,
+        y: 0,
+        w: 24,
+        h: 33,
+      },
+      sourceSize: {
+        w: 24,
+        h: 33,
+      },
+    },
+    {
+      filename: "tile004.png",
+      frame: {
+        x: 27,
+        y: 36,
+        w: 22,
+        h: 33,
+      },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: {
+        x: 0,
+        y: 0,
+        w: 22,
+        h: 33,
+      },
+      sourceSize: {
+        w: 22,
+        h: 33,
+      },
+    },
+    {
+      filename: "tile005.png",
+      frame: {
+        x: 51,
+        y: 36,
+        w: 21,
+        h: 33,
+      },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: {
+        x: 0,
+        y: 0,
+        w: 21,
+        h: 33,
+      },
+      sourceSize: {
+        w: 21,
+        h: 33,
+      },
+    },
+    {
+      filename: "tile006.png",
+      frame: {
+        x: 69,
+        y: 1,
+        w: 22,
+        h: 33,
+      },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: {
+        x: 0,
+        y: 0,
+        w: 22,
+        h: 33,
+      },
+      sourceSize: {
+        w: 22,
+        h: 33,
+      },
+    },
+    {
+      filename: "tile007.png",
+      frame: {
+        x: 74,
+        y: 36,
+        w: 23,
+        h: 33,
+      },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: {
+        x: 0,
+        y: 0,
+        w: 23,
+        h: 33,
+      },
+      sourceSize: {
+        w: 23,
+        h: 33,
+      },
+    },
+  ],
+  meta: {
+    app: "http://www.codeandweb.com/texturepacker",
+    version: "1.0",
+    image: "spritesheet.png",
+    format: "RGBA8888",
+    size: {
+      w: 98,
+      h: 70,
+    },
+    scale: "1",
+  },
+};
